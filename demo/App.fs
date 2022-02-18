2d1097b55ca84f370eba8bdca57fcf7e2db3758f
Denis Bobrov <delneg@yandex.ru>
2022-02-18 19:36:09 +0300

    Added stub demo app, move Fable.Popper.fsproj to src/

---
+++ demo/App.fs
@@ -0,0 +1,25 @@
+﻿namespace Fable.Popper.Demo
+
+open Feliz
+open Browser.Dom
+open Fable.Core.JsInterop
+   
+
+
+module App =
+    [<ReactComponent>]
+    let HelloWorld() =
+        let (count, setCount) = React.useState(0)
+        Html.div [
+            Html.h1 count
+            Html.button [
+                prop.onClick (fun _ -> setCount(count + 1))
+                prop.text "Increment"
+            ]
+        ]
+    
+
+    ReactDOM.render(
+        HelloWorld(),
+        document.getElementById "app"
+    )
